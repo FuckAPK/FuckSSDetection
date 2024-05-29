269fbf65f8afb09cfaaa4b81b1087bd5cef8894c
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-05-29 21:56:40 +0800

    update: add log

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.baiyu.fuckssdetection"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         resourceConfigurations += "en"
         vectorDrawables.useSupportLibrary = true
     }
--- app/src/main/java/org/baiyu/fuckssdetection/MainHook.kt
+++ app/src/main/java/org/baiyu/fuckssdetection/MainHook.kt
@@ -1,7 +1,9 @@
 package org.baiyu.fuckssdetection
 
+import android.content.Intent
+import android.os.IBinder
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
@@ -20,7 +22,23 @@ class MainHook : IXposedHookLoadPackage {
             XposedBridge.hookAllMethods(
                 activityTaskManagerServiceClazz,
                 "registerScreenCaptureObserver",
-                XC_MethodReplacement.returnConstant(null)
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val activityRecordClazz = XposedHelpers.findClass(
+                            "com.android.server.wm.ActivityRecord",
+                            lpparam.classLoader
+                        )
+                        val ar = XposedHelpers.callStaticMethod(
+                            activityRecordClazz,
+                            "forTokenLocked",
+                            arrayOf(IBinder::class.java),
+                            param.args[0]
+                        )
+                        val arIntent = XposedHelpers.getObjectField(ar, "intent") as Intent
+                        XposedBridge.log("prevent screenshot detection register from ${arIntent.component?.flattenToString()}")
+                        param.result = null
+                    }
+                }
             )
         } catch (t: Throwable) {
             XposedBridge.log(t)
