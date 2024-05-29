ee366cc4fa7c8f1461c8e743eb773023000c2887
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-05-29 14:32:57 +0800

    init

---
+++ app/src/main/java/org/baiyu/fuckssdetection/MainHook.kt
@@ -0,0 +1,29 @@
+package org.baiyu.fuckssdetection
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: LoadPackageParam) {
+        if (lpparam.packageName == BuildConfig.APPLICATION_ID) {
+            return
+        }
+
+        try {
+            val activityTaskManagerServiceClazz = XposedHelpers.findClass(
+                "com.android.server.wm.ActivityTaskManagerService",
+                lpparam.classLoader
+            )
+            XposedBridge.hookAllMethods(
+                activityTaskManagerServiceClazz,
+                "registerScreenCaptureObserver",
+                XC_MethodReplacement.returnConstant(null)
+            )
+        } catch (t: Throwable) {
+            XposedBridge.log(t)
+        }
+    }
+}
